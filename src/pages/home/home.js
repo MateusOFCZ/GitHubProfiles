32410b18ba12faf53c80ff9f6f7f139668fba3aa
MateusOFCZ <mateusdacruz357@hotmail.com>
2022-06-27 22:13:50 -0300

    🎨Animations

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,52 +4,66 @@ import moment from 'moment';
 import 'moment/locale/pt-br';
 import Config from '../../assets/config.json';
 import { useParams } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import './styles.css';
 
 export default function Home() {
+  const EmphasisControls = useAnimation();
   let { username } = useParams();
-  if (!username) {
-    username = Config.GITHUB_USERNAME;
+  const [Data, setData] = useState();
+  const [Username, setUsername] = useState(username || Config.GITHUB_USERNAME);
+
+  const ChangeEphasis = () => {
+    EmphasisControls.start({ color: ['#FFFFFF', '#0061BB', '#FFFFFF', '#0061BB', '#FFFFFF'], transition: { duration: 1 } });
+
+    setTimeout(() => {
+      EmphasisControls.stop();
+      document.getElementById('username_input').focus();
+    }, 1000);
   }
 
-  const CardVariants = {
-    visible: { scale: 1, transition: { duration: 1 } },
-    hidden: { scale: 0, transition: { duration: 1 } }
-  };
-
-  const [Data, setData] = useState();
+  const Search = (Username) => {
+    if (Username) {
+      axios.get(`https://api.github.com/users/${Username}/repos?type=owner&sort=updated`)
+        .then(function (response) {
+          setData(response.data);
+          setUsername(Username);
+        }).catch(function (err) {
+          console.error(err.message);
+          console.warn(`User ${Username} not found.`);
+          setData(null);
+          setTimeout(() => {
+            Search(Username);
+          }, 5000);
+        });
+    }
+  }
 
   if (!Data) {
-    setInterval(() => {
-      if (Data == null) {
-        axios.get(`https://api.github.com/users/${username}/repos?type=owner&sort=updated`)
-          .then(function (response) {
-            setData(response.data);
-          }).catch(function (err) {
-            console.error(err.message);
-            console.warn(`User ${username} not found.`)
-            setData(null);
-          });
-      }
+    setTimeout(() => {
+      Search(Username);
     }, 5000);
   }
 
   return (
-    <motion.div className="Home" transition={{ duration: 10, repeat: Infinity, repeatDelay: 0 }} animate={{ backgroundPosition: ['0%', '50%', '100%', '50%', '0%'] }}>
+    <motion.div className="Home" transition={{ duration: 10, repeat: Infinity, repeatDelay: 0 }} animate={{ backgroundPosition: ['0%', '100%', '0%'] }}>
       {!!Data ?
         <motion.div transition={{ duration: 2, delay: 0.5 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-          <motion.div className='panel'  transition={{ duration: 2, delay: 1 }} animate={{ boxShadow:'0 0 1vw 0.6vw #FFFFFF' }}>
+          <motion.span className='information' onClick={(e) => ChangeEphasis()} transition={{ duration: 1, delay: 1.5 }} initial={{ x: '-110%', opacity: 0 }} animate={{ opacity: 1, x: '2%' }}>
+            <FontAwesomeIcon icon={faInfoCircle} className='icon' /> <span className='text'>Você pode pesquisar pelo username clicando no <b>título</b>.</span>
+          </motion.span>
+          <motion.div className='panel' transition={{ duration: 10, repeat: Infinity, repeatDelay: 0 }} animate={{ boxShadow: ['0 0 1vw 0.1vw #FFFFFF', '0 0 1vw 0.6vw #FFFFFF', '0 0 1vw 0.1vw #FFFFFF'] }}>
             <div className='header'>
-              <motion.p transition={{ duration: 0.5, delay: 1 }} initial={{ y: '-350%' }} animate={{ y: '0%' }}>
-                <motion.p transition={{ duration: 0.5 }} whileHover={{ scale: 1.1 }}>
-                  <a href={`https://github.com/${username}`} target='_blank'>{username}</a>
-                </motion.p>
-              </motion.p>
+              <motion.div transition={{ duration: 0.5, delay: 1 }} initial={{ y: '-350%' }} animate={{ y: '0%' }}>
+                <motion.span animate={EmphasisControls}>
+                  <input id='username_input' className='username_input' type='text' defaultValue={Username} onBlur={(e) => Search(e.target.value)} onKeyPress={(e) => e.key === 'Enter' && Search(e.target.value)}></input>
+                </motion.span>
+              </motion.div>
             </div>
             <motion.div className='card_list' transition={{ duration: 2, delay: 1.5 }} initial={{ y: '350%' }} animate={{ y: '0%' }}>
-              {Object.keys(Data).map((Key, Index) => (
+              {Object.keys(Data).map(Key => (
                 <motion.div className='card' transition={{ duration: 0.5 }} whileHover={{ scale: 1.05 }} viewport={{ once: false }} initial={{ scale: 0, opacity: 0 }} whileInView={{ scale: 1, opacity: 1 }}>
                   <a className='title' href={Data[Key].html_url} target='_blank'>{Data[Key].name}</a>
                   <div>
